9fd465b00ad0957f87de16286e7390eafc3cc5c5
Ryan Yogan <ryanyogan@gmail.com>
2018-05-10 15:58:33 -0500

    Configued the basic ApolloClient

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import registerServiceWorker from './registerServiceWorker';
+
+import './style.css';
+import App from './App';
+
+// TODO: Move to constants dir
+const tempUri = 'https://api.github.com/graphql';
+
+const httpLink = new httpLink({
+  uri: tempUri,
+  headers: {
+    authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`
+  }
+});
+
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
