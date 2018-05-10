754e99597836017241fc153e2f418a1342134c03
Ryan Yogan <ryanyogan@gmail.com>
2018-05-10 16:49:23 -0500

    Added link-retry, this will attempt 5 more times to make a call with time decay

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { ApolloClient } from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
+import { RetryLink } from 'apollo-link-retry';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -33,7 +34,19 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
   }
 });
 
-const link = ApolloLink.from([errorLink, httpLink]);
+const retryLink = new RetryLink({
+  delay: {
+    initial: 500,
+    max: Infinity,
+    jitter: true // randomize initialDecay
+  },
+  attempts: {
+    max: 5,
+    retryIf: ({ error }) => !!error
+  }
+});
+
+const link = ApolloLink.from([errorLink, retryLink, httpLink]);
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
