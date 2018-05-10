d0047c3a9dd12e42120ce3a9759452824c16bdf4
Ryan Yogan <ryanyogan@gmail.com>
2018-05-10 16:10:47 -0500

    Wiring up the first render prop pattern query component

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
-const Profile = () => <div>Profile Stub</div>;
+const GET_CURRENT_USER = gql`
+  {
+    viewer {
+      login
+      name
+    }
+  }
+`;
+
+const Profile = () => (
+  <Query query={GET_CURRENT_USER}>
+    {() => <div>Profile, notice the Render Prop pattern?</div>}
+  </Query>
+);
 
 export default Profile;
